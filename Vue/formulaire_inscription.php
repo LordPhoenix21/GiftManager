a2cfd3c50aff67c927e61fd7cd171e2aad39d551
Nanin <lucaspayet@orange.fr>
2018-12-27 15:33:42 +0100

    Index + css
    Pas fini du tout c'est moche

---
+++ Vue/formulaire_inscription.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset = "utf-8"> 
+        <title>GiftManager</title>
+        <link rel = "stylesheet" href = "css.css">
+    </head>
+    <body>
+        <h1>Bienvenue sur Gift Manager</h1>
+        <h2>Inscription</h2>
+        <form method = "post" action ="inscription.php">
+            Nom d'utilisateur : <input type = "text" name "utilisateur"></br>
+            Adresse mail : <input type = "text" name "mail"></br>
+            Mot de passe : <input type = "text" name "mdp"></br>
+            Confirmez Mot de passe : <input type = "text" name "mdpConfirm"></br>
+            <input type = "submit" value ="Inscription">
+        </form>
+    </body>
+</html>
