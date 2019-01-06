9a0b1950a41faa7ffd6f39e4343e2fe8065d8ed3
Nanin <lucaspayet@orange.fr>
2019-01-06 12:26:08 +0100

    Page interne + header
    Pas encore de mise en page c'est juste fonctionnel

---
+++ Vue/page_interne.php
@@ -0,0 +1,26 @@
+<?php
+    session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset = "utf-8"> 
+        <title>GiftManager</title>
+        <link rel = "stylesheet" href = "css.css">
+    </head>
+    <header>
+        <img src="Logo.png" class = "logo" alt="Logo" height="2%" width="2%">
+        <div class = "header-liens"> 
+            <a href ="groupe.php"> Vos Groupes </a>
+            <a href = "liste.php"> Vos Listes </a>
+        </div>
+        <div class = "header-droite">
+            <a href ="parametre.php"> Parametre </a>
+        </div> 
+    </header>
+    <body>
+        Test
+        
+    </body>
+</html>
